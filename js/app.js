27b49195b2da0563346e9d4248e56375ee7397bd
Reahana <reahana233@gmail.com>
2023-04-02 18:20:53 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-const loadPhones = async() =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadPhones = async(searchText, dataLimit) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
-const displayPhones = (phones) =>{
+
+const displayPhones = (phones, dataLimit) =>{
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.textContent = '';
+    // display 10 phones only 
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+    
+
+    // display no phones found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+    // display all phones
     phones.forEach(phone =>{
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -23,5 +44,65 @@ const displayPhones = (phones) =>{
         `;
         phonesContainer.appendChild(phoneDiv);
     });
+    // stop spinner or loader
+    toggleSpinner(false);
 }
-loadPhones();
+
+const processSearch = (dataLimit) =>{
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+// handle search button click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // start loader
+    processSearch(10);
+})
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+});
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+
+// not the best way to load show All
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+})
+
+const loadPhoneDetails = async id =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    console.log(phone.mainFeatures.sensors[0]);
+    phoneDetails.innerHTML = `
+        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+        <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information '}</p>
+        <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
+        <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
+    `
+}
+
+loadPhones('apple');
